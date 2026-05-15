4e288fb1277d74ddf83033839e24639ed23b46a3
Heleno <helenojose1708@gmail.com>
2026-05-15 19:19:56 -0300

    Revert "Mapeamento do banco de dados no Django"
    This reverts commit 579ae254383a682ff131bb50bca8b9e636aca9d0.

--- config/settings.py
+++ config/settings.py
@@ -73,21 +73,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'clinica_odontologica',  
-        'USER': '',   
-        'PASSWORD': '', 
-        'HOST': '127.0.0.1',             
-        'PORT': '3306',                    
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
