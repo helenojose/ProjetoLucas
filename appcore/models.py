15b21c7940601dc7da58adbe2448cd69dffe3ffc
Natan José de Araújo <natanjaraujo2005@gmail.com>
2026-05-18 01:02:57 -0300

    Ajustes no banco de dados

--- appcore/models.py
+++ appcore/models.py
@@ -2,7 +2,8 @@
 
 # 1. TABELA: usuario_sistema
 class UsuarioSistema(models.Model):
-    email = models.EmailField(unique=True)
+    cod_usuario_sistema = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=255, unique=True)
     senha = models.CharField(max_length=255)
     data_cadastro = models.DateField(auto_now_add=True)
     status = models.IntegerField(default=1, choices=[(1, 'Ativo'), (0, 'Inativo')])
@@ -13,6 +14,7 @@ class UsuarioSistema(models.Model):
 
 # 2. TABELA: especialidade
 class Especialidade(models.Model):
+    cod_especialidade = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=100, unique=True)
     data_cadastro = models.DateField(auto_now_add=True)
     status = models.IntegerField(default=1, choices=[(1, 'Ativo'), (0, 'Inativo')])
@@ -23,12 +25,25 @@ class Especialidade(models.Model):
     class Meta:
         db_table = 'especialidade'
 
+# 3. TABELA: procedimento
+class Procedimento(models.Model):
+    cod_procedimento = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100, unique=True)
+    data_cadastro = models.DateField(auto_now_add=True)
+    status = models.IntegerField(default=1, choices=[(1, 'Ativo'), (0, 'Inativo')])
 
-# 3. TABELA: dentista
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        db_table = 'procedimento'
+
+# 4. TABELA: dentista
 class Dentista(models.Model):
+    cod_dentista = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=15, unique=True)
     especialidade = models.ForeignKey(Especialidade, on_delete=models.PROTECT)
-    telefone = models.CharField(max_length=11, unique=True)
     data_cadastro = models.DateField(auto_now_add=True)
     status = models.IntegerField(default=1, choices=[(1, 'Ativo'), (0, 'Inativo')])
 
@@ -38,10 +53,15 @@ class Dentista(models.Model):
     class Meta:
         db_table = 'dentista'
 
-
-# 4. TABELA: servico
-class Servico(models.Model):
-    nome = models.CharField(max_length=255, unique=True)
+# 5. TABELA: paciente
+class Paciente(models.Model):
+    cod_paciente = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=14, unique=True)
+    data_nascimento = models.DateField()
+    telefone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=255, unique=True)
+    endereco = models.CharField(max_length=255)
     data_cadastro = models.DateField(auto_now_add=True)
     status = models.IntegerField(default=1, choices=[(1, 'Ativo'), (0, 'Inativo')])
 
@@ -49,47 +69,37 @@ class Servico(models.Model):
         return self.nome
 
     class Meta:
-        db_table = 'servico'
-
-class Paciente(models.Model):
-    nome_paciente = models.CharField(max_length=255)
-    cpf = models.CharField(max_length=14, unique=True)
-    data_nascimento = models.DateField()
-    telefone = models.CharField(max_length=15)
-    email = models.EmailField(unique=True)
-    endereco = models.TextField()
-    data_cadastro = models.DateField(auto_now_add=True)
-    status = models.IntegerField(default=1, choices=[(1, 'Ativo'), (0, 'Inativo')])
-
-    def __str__(self):
-        return self.nome_paciente
-
-    class Meta:
         db_table = 'paciente'
 
-# 5. TABELA: agendamento
+# 6. TABELA: agendamento
 class Agendamento(models.Model):
-    STATUS_CONSULTA_CHOICES = [
-        ('AGENDADO', 'Agendado'),
-        ('CONCLUIDO', 'Concluído'),
-        ('CANCELADO', 'Cancelado'),
-    ]
+    # Definindo as escolhas como números
+    class StatusAgendamento(models.IntegerChoices):
+        AGENDADO  = 1, 'AGENDADO'
+        CONCLUIDO = 2, 'CONCLUIDO'
+        CANCELADO = 3, 'CANCELADO'
 
-    # Relacionamento com a tabela paciente
-    paciente = models.ForeignKey(Paciente, on_delete=models.PROTECT)
+    cod_agendamento = models.AutoField(primary_key=True)
 
     # Dados do atendimento
     data_consulta = models.DateField()
     hora_consulta = models.TimeField()
-    status_consulta = models.CharField(max_length=20, choices=STATUS_CONSULTA_CHOICES, default='AGENDADO')
-    observacoes = models.TextField(blank=True, null=True)
+    
+    # Campo configurado com a classe StatusAgendamento
+    status_agendamento = models.IntegerField(
+        choices=StatusAgendamento.choices, 
+        default=StatusAgendamento.AGENDADO
+    )
+    
+    motivo_cancelamento = models.TextField(blank=True, null=True)
+    observacao = models.TextField(blank=True, null=True)
 
-    # Relacionamentos com os profissionais e procedimentos
+    # Relacionamentos
     dentista = models.ForeignKey(Dentista, on_delete=models.PROTECT)
-    servico = models.ForeignKey(Servico, on_delete=models.PROTECT)
+    paciente = models.ForeignKey(Paciente, on_delete=models.PROTECT)
+    procedimento = models.ForeignKey(Procedimento, on_delete=models.PROTECT)
 
     data_cadastro = models.DateField(auto_now_add=True)
-    status = models.IntegerField(default=1, choices=[(1, 'Ativo'), (0, 'Inativo')])
 
     class Meta:
         db_table = 'agendamento'
